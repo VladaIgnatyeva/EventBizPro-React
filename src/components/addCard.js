4afbe5244cd8b1eea71ac8b7a757c3c5e8501c5c
VladaIgnatyeva <vlada.ignatyeva7@gmail.com>
2020-05-18 18:30:01 +0300

    pwa 1.1

---
+++ src/components/addCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Button, Card, Container } from 'react-bootstrap';
+
+class AddCard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Card id="card">
+                <Card.Img variant="top" src={img_default} />
+                <Card.Body>
+                    <Card.Title>{this.props.item.title}</Card.Title>
+                    <Card.Text>
+                        {this.props.item.text}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">{this.props.item.footer}</small>
+                </Card.Footer>
+            </Card >
+        )
+    }
+}
+
+export default AddCard;
