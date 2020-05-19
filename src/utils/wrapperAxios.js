753d6049c711be4bb5f8a694cea5b22474cfafa5
VladaIgnatyeva <vlada.ignatyeva7@gmail.com>
2020-05-19 22:05:56 +0300

    change 'localhost'

--- src/utils/wrapperAxios.js
+++ src/utils/wrapperAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const api = process.env.REACT_APP_API_URL
-const api = 'http://localhost:8080';
+const api = process.env.REACT_APP_API_URL
+//const api = 'http://localhost:8080';
 export default class WrapperAxios {
 
     post(url, data) {
